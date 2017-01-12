ac2ba75f4fe7f393a834c5d81a762851c7dd4683
Joni Eskelinen <joni@mobie.fi>
2017-01-12 10:00:36 +0200

    Clean variable names

--- scriptrun.cmd
+++ scriptrun.cmd
@@ -1,28 +1,33 @@
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
-
 REM check that first argument is a file
 IF NOT EXIST %1 (
 	ECHO %1 doesn't exists
 	EXIT /B 1
 )
+REM Cygwin installation directory
+SET CYGPATH=C:\cygwin
+REM add Cygwin paths to environment
+SET PATH=%CYGPATH%\usr\local\bin;%CYGPATH%\bin;%PATH%
 REM compose arguments to A[] array
-SET i=1
+SET argc=0
 FOR %%x IN (%*) DO (
+	SET /A argc+=1
 	REM if argument is a file
 	IF EXIST %%x (
 		REM translate to Cygwin path
-		FOR /F "delims= usebackq" %%F IN (`C:\cygwin\bin\cygpath.exe %%x`) DO (
-			SET arg="%%F"
+		FOR /F "delims= usebackq" %%F IN (`cygpath.exe %%x`) DO (
+			SET _arg="%%F"
 		)
 	) ELSE (
-		SET arg=%%x
+		SET _arg=%%x
 	)
-	SET A[!i!]=!arg!
-	SET /A i+=1
+	SET argv[!argc!]=!_arg!
 )
-REM add Cygwin paths to environment
-SET PATH=C:\cygwin\usr\local\bin;C:\cygwin\usr\bin;%PATH%
+REM unset local variables
+SET "_i="
+SET "_arg="
 REM run script in Cygwin
-START "Cygwin" "C:\cygwin\bin\mintty.exe" --exec ^
-	!A[1]! !A[2]! !A[3]! !A[4]! !A[5]! !A[6]! !A[7]! !A[8]! !A[9]!
+START "Cygwin" "mintty.exe" --exec !argv[1]! ^
+	!argv[2]! !argv[3]! !argv[4]! !argv[5]! ^
+	!argv[6]! !argv[7]! !argv[8]! !argv[9]!
