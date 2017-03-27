3ef46e179c6b1be1912255fe73d08e68932913d2
Joni Eskelinen <joni@mobie.fi>
2017-03-27 14:43:02 +0300

    Fix arch checking
    * Search for /bin/uname instead of relying on PATH

--- setup-cygwinports.sh
+++ setup-cygwinports.sh
@@ -6,8 +6,12 @@
 
 # script's directory
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+pushd . >/dev/null
+# find /bin directory
+while [[ "$PWD" != '/' ]] && [[ ! -f "$PWD/bin/uname" ]]; do cd ..; done
 # check whether 64-bit or 32-bit installation
-[[ $(uname -m) == "x86_64" ]] &&
+[[ $(./bin/uname -m) == "x86_64" ]] &&
     SETUP="$DIR/setup-x86_64.exe" || SETUP="$DIR/setup-x86.exe"
+popd >/dev/null
 # start setup in Windows
 CMD /C "START $(cygpath -w "$SETUP") -K http://cygwinports.org/ports.gpg"
