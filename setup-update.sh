5fd7d4f4c86b8b975642478a56932921f4147cf0
Joni Eskelinen <joni@mobie.fi>
2017-04-05 11:14:02 +0300

    Remove obsolete tools

--- setup-update.sh
+++ setup-update.sh
@@ -2,25 +2,26 @@
 
 # script's directory
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-# temporary file for download
-TMP=$(mktemp)
-# check whether 64-bit or 32-bit installation
-[[ $(uname -m) == "x86_64" ]] &&
-    FILENAME="setup-x86_64.exe" || FILENAME="setup-x86.exe"
-# destination filename
-DEST="$DIR/$FILENAME"
 # check that wget is installed
 cygcheck wget &>/dev/null
 if [[ $? -ne 0 ]]; then
-    echo "Please install wget.";
-    read -n 1 -s
+    echo "Please install wget."
     exit 1
 fi
+pushd . >/dev/null
+# find /bin directory
+while [[ "$PWD" != '/' ]] && [[ ! -f "$PWD/bin/uname" ]]; do cd ..; done
+# check whether 64-bit or 32-bit installation
+[[ $(./bin/uname -m) == "x86_64" ]] &&
+    FILENAME="setup-x86_64.exe" || FILENAME="setup-x86.exe"
+popd >/dev/null
+# destination filename
+DEST="$DIR/$FILENAME"
 # download to temporary file
+TMP=$(mktemp)
 wget -O $TMP "https://cygwin.com/$FILENAME"
 if [ $? -ne 0 ]; then
-    echo "Failed to load $FILENAME."
-    read -n 1 -s
+    echo "Failed to download $FILENAME."
     exit 1
 fi
 # move to destination
